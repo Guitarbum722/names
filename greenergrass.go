192901e641c02714b9e55b5543a6cec9606fadc9
guitarbum722 <johnkenneth.moore@gmail.com>
2017-02-26 20:05:44 -0700

    Put Name struct fields on one line

--- greenergrass.go
+++ greenergrass.go
@@ -10,11 +10,7 @@ const testVersion = 1
 
 // Name contains a common list fields that could be combined as a person's full name
 type Name struct {
-	first  string
-	middle string
-	last   string
-	prefix string
-	suffix string
+	first, middle, last, prefix, suffix string
 }
 
 // SeparateName accepts a name n as input, and parses it according to common logic and returns a Name struct
